aef0811dbea85f337a4746db30d7419ed45d3c46
sadiya <sadiya@sadiyavbox.org>
2023-12-31 11:17:21 +0530

    Committer: sadiya <sadiya@sadiyavbox.org>  On branch main  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const fetchData = () => {
+    fetch(`http://127.0.0.1:8000/logs`)
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const startFetching = () => {
+    if (!intervalId) {
+      fetchData();
+      const id = setInterval(fetchData, 5000); // fetch every 5 seconds
+      setIntervalId(id);
+    }
+  };
+
+  const stopFetching = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [intervalId]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 style={{ color: '#00FFEF' }}>Access Logs </h2>
+      <button onClick={startFetching}>Start Fetching</button>
+      <button onClick={stopFetching}>Stop Fetching</button>
+
+      <ul className="responsive-table">
+        <li className="table-header">
+          <div className="col col-1">IP Address</div>
+          <div className="col col-2">Date and Time</div>
+        </li>
+        {data.map((item, index) => (
+          <li className="table-row" key={index}>
+            <div className="col col-1" data-label="IP Address">
+              {item.ip_address}
+            </div>
+            <div className="col col-2" data-label="Date and Time">
+              {item.date_time}
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+  /*un comment for OG bruhhhhh!
+  return (
+    <div className="App">
+      <h1>Access Logs</h1>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>IPaddress</th>
+              <th>DateandTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.ip_address}</td>
+                <td>{item.date_time}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}*/
