573fe8a3cc54d93d51f37c6b6fca6875998fd81d
Sadiya Fathima <134277494+stormothecyborg@users.noreply.github.com>
2023-12-31 14:01:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,96 +1,64 @@
-import { useEffect, useState } from "react";
+// App.js
+
+import React, { useState } from 'react';
 import "./App.css";
 
-function App() {
-  const [data, setData] = useState([]);
-  const [intervalId, setIntervalId] = useState(null);
+const App = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const fetchData = () => {
-    fetch(`http://127.0.0.1:8000/logs`)
-      .then((response) => response.json())
-      .then((actualData) => {
-        console.log(actualData);
-        setData(actualData);
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
+  const handleQuery = async () => {
+    try {
+      setLoading(true);
+      const now = new Date();
+      const startDateTime = now.toISOString(); // Curren t date and time
+      const endDateTime = now.toISOString(); 
 
-  const startFetching = () => {
-    if (!intervalId) {
-      fetchData();
-      const id = setInterval(fetchData, 5000); // fetch every 5 seconds
-      setIntervalId(id);
-    }
-  };
-
-  const stopFetching = () => {
-    if (intervalId) {
-      clearInterval(intervalId);
-      setIntervalId(null);
-    }
-  };
-
-  useEffect(() => {
-    return () => {
-      if (intervalId) {
-        clearInterval(intervalId);
+      // Replace with the actual URL of your FastAPI server
+      const response = await fetch('http://localhost:8000/logs?start_time=${startDateTime}&end_time=${endDateTime}');
+      
+      if (!response.ok) {
+        throw new Error('Failed to fetch logs');
       }
-    };
-  }, [intervalId]);
+
+      const data = await response.json();
+      console.log('Data from server:', data);
+
+      setLogs(data.logs);
+      console.log('Logs in state:', logs);
+
+
+    } catch (error) {
+      console.error('Error fetching logs:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="container">
-      <h2 style={{ color: '#00FFEF' }}>Access Logs </h2>
-      <button onClick={startFetching}>Start Fetching</button>
-      <button onClick={stopFetching}>Stop Fetching</button>
+      <h1>FastAPI Log Viewer</h1>
 
-      <ul className="responsive-table">
-        <li className="table-header">
-          <div className="col col-1">IP Address</div>
-          <div className="col col-2">Date and Time</div>
-        </li>
-        {data.map((item, index) => (
-          <li className="table-row" key={index}>
-            <div className="col col-1" data-label="IP Address">
-              {item.ip_address}
-            </div>
-            <div className="col col-2" data-label="Date and Time">
-              {item.date_time}
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+      <div>
+        <button onClick={handleQuery} disabled={loading}>
+          {loading ? 'Loading...' : 'Query FastAPI for Logs'}
+        </button>
+      </div>
 
-export default App;
-
-  /*un comment for OG bruhhhhh!
-  return (
-    <div className="App">
-      <h1>Access Logs</h1>
-      <div className="table-container">
-        <table>
-          <thead>
-            <tr>
-              <th>IPaddress</th>
-              <th>DateandTime</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => (
-              <tr key={index}>
-                <td>{item.ip_address}</td>
-                <td>{item.date_time}</td>
-              </tr>
+      <div>
+        <h2>Logs:</h2>
+        {logs.length > 0 ? (
+          <ul>
+            {logs.map((log, index) => (
+              <li key={index}>{log}</li>
             ))}
-          </tbody>
-        </table>
+          </ul>
+        ) : (
+          <p>No logs to display.</p>
+        )}
       </div>
     </div>
   );
-}*/
+};
+
+export default App;
